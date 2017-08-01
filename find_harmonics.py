919b9e8143333b8c09f9f53f5a23f385178d2f98
sstaehler <simon.staehler@erdw.ethz.ch>
2017-08-01 12:07:31 +0200

    Improvements in interface find_harmonics.py
     - better description of parameters
     - create output dir automatically

--- find_harmonics.py
+++ find_harmonics.py
@@ -12,6 +12,7 @@
 from tqdm import tqdm
 import argparse
 import glob
+import os
 from calc_spec_utils import calc_spec
 
 
@@ -20,46 +21,51 @@ def define_arguments():
     parser = argparse.ArgumentParser(description=helptext)
 
     helptext = 'Path to Seismogram files (can be anything that Obspy reads)'
-    parser.add_argument('--smgr_path', help=helptext)
+    parser.add_argument('smgr_path', help=helptext)
 
-    helptext = 'Number of harmonics'
+    helptext = 'Number of harmonics (default: 4)'
     parser.add_argument('--nharms', help=helptext, type=int, default=4)
 
-    helptext = 'Frequency range (minimum)'
+    helptext = 'Minimum frequency in Hz (default: 0.4)'
     parser.add_argument('--fmin', help=helptext, type=float, default=0.4)
 
-    helptext = 'Frequency range (maximum)'
+    helptext = 'Maximum frequency in Hz (default: 4.0)'
     parser.add_argument('--fmax', help=helptext, type=float, default=4.0)
 
-    helptext = 'Plot range in dB (maximum), choose larger value for' + \
-            'hydrophone channel (i.e. 40)'
-    parser.add_argument('--vmax', help=helptext, type=float, default=-60.0)
-
-    helptext = 'Plot range in dB (minimum), choose larger value for' + \
+    helptext = 'Minimum of plot range in dB (default: -160), choose larger value for' + \
             'hydrophone channel (i.e. -60)'
     parser.add_argument('--vmin', help=helptext, type=float, default=-160.0)
 
-    helptext = 'Output directory'
+    helptext = 'Maximum of plot range in dB (default: -60), choose larger value for' + \
+            'hydrophone channel (i.e. 40)'
+    parser.add_argument('--vmax', help=helptext, type=float, default=-60.0)
+
+    helptext = 'Output directory (default: ''.'')'
     parser.add_argument('--out_path', help=helptext, default='.')
 
     return parser
 
 
 def main():
+    # Define and parse input arguments
     parser = define_arguments()
-
-    # Parse input arguments
     args = parser.parse_args()
 
+    # Test whether output directories exist and create, if necessary
+    dir_Picks = os.path.join(args.out_path, 'Picks')
+    os.makedirs(dir_Picks, exist_ok=True)
+    dir_Specs = os.path.join(args.out_path, 'Spectrograms')
+    os.makedirs(dir_Specs, exist_ok=True)
+    
+    # Create list of all seismogram files
     files = glob.glob(args.smgr_path)
     if (len(files) == 0):
         print('Did not find any files in path: %s' % args.smgr_path)
         raise ValueError()
-
     files.sort()
 
+    # Calc spectrograms and pick maxima for all seismogram files
     for fnam_smgr in tqdm(files):
-
         calc_spec(fnam_smgr, fmin=1e-2, fmax=10,
                   vmin=args.vmin, vmax=args.vmax, winlen=300,
                   pick_harmonics=True, 
